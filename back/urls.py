104d9d88858a2d3a8c11c0b4ba30cfae5eac01e2
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-04-26 14:16:35 -0400

    PedidoViewSet

--- back/urls.py
+++ back/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 
-from back.views import ProductoViewSet, TiendaViewSet, BoletaViewSet, CategoriaViewSet
+from back.views import ProductoViewSet, TiendaViewSet, BoletaViewSet, CategoriaViewSet, PedidoViewSet
 
 router = DefaultRouter()
 
@@ -8,3 +8,4 @@ router.register(r'producto', ProductoViewSet, basename='Producto')
 router.register(r'tienda', TiendaViewSet, basename='Tienda')
 router.register(r'boleta', BoletaViewSet, basename='Boleta')
 router.register(r'categoria', CategoriaViewSet, basename='Categoria')
+router.register(r'pedido', PedidoViewSet, basename='Pedido')
