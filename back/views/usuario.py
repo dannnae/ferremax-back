b954a1de65ae6543b322d09555815379831e06d8
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-04-27 17:47:33 -0400

    arreglo password

--- back/views/usuario.py
+++ back/views/usuario.py
@@ -1,12 +1,22 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, CharField
 from back.models import CustomUser
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password', 'type'] 
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        hashed_password = make_password(password)
+        user = CustomUser.objects.create(password=hashed_password, **validated_data)
+
+        return user
 
 class UserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
