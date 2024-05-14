ff02c2fe4ee42c71fdcca625961a301c0abce034
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-13 20:48:15 -0400

    endpoint webpay plus

--- back/views/boleta.py
+++ back/views/boleta.py
@@ -1,5 +1,11 @@
+from transbank.webpay.webpay_plus.transaction import Transaction
+import uuid
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import redirect
 
 from back.models import Boleta
 
@@ -12,4 +18,25 @@ class BoletaSerializer(ModelSerializer):
 class BoletaViewSet(ModelViewSet):
     queryset = Boleta.objects.all()
     serializer_class = BoletaSerializer
+    
+    def generate_buy_order(self):
+        return str(uuid.uuid4().int)[:10]
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        transaccion = (Transaction()).create(
+            buy_order=self.generate_buy_order(), 
+            session_id=self.generate_buy_order(), 
+            amount=response.data['valor_total'], 
+            return_url='http://localhost:8000/api/boleta/commit/'
+        )
+
+        return Response(transaccion)
+    
+    @action(detail=False, methods=['POST'])
+    def commit(self, request):
+        token = request.data.get('token_ws')
+        resultado = (Transaction()).commit(token=token)
+
+        return redirect('http://localhost:4200/pago_confirmado/')
