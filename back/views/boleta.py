d16ceccfd1680c4f0773f9c87022fe424720d0b6
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-16 18:07:44 -0400

    decorators swagger

--- back/views/boleta.py
+++ back/views/boleta.py
@@ -1,6 +1,8 @@
 from transbank.webpay.webpay_plus.transaction import Transaction
 import uuid
 import requests
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
@@ -13,7 +15,7 @@ from back.models import Boleta
 class BoletaSerializer(ModelSerializer):
     class Meta:
         model = Boleta
-        fields = '__all__'
+        fields = ['__all__']
 
 
 class BoletaViewSet(ModelViewSet):
@@ -22,7 +24,11 @@ class BoletaViewSet(ModelViewSet):
     
     def generate_buy_order(self):
         return str(uuid.uuid4().int)[:10]
-
+    
+    @swagger_auto_schema(
+        operation_description="Crea una nueva Boleta e inicia una transacción",
+        responses={200: "Respuesta HTML con el formulario WebPay", 404: "Transacción fallida"}
+    )
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         
@@ -44,6 +50,13 @@ class BoletaViewSet(ModelViewSet):
 
         return Response(404)
     
+    @swagger_auto_schema(
+        operation_description="Confirmar una transacción y actualizar el estado del pago",
+        manual_parameters=[
+            openapi.Parameter('token_ws', openapi.IN_QUERY, description="Token transaccion", type=openapi.TYPE_STRING)
+        ],
+        responses={302: "Redirigir a la página de confirmación o rechazo de pago"}
+    )
     @action(detail=False, methods=['GET'])
     def commit(self, request):
         token = request.query_params.get('token_ws')
