64c3599f927e24f9ae63582844cc155503203de8
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-16 18:08:11 -0400

    cambio dolar

--- back/views/producto.py
+++ back/views/producto.py
@@ -1,12 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from django.conf import settings
 
 from back.models import Producto
 
 class ProductoSerializer(ModelSerializer):
+    valor_usd = SerializerMethodField()
+
     class Meta:
         model = Producto
         fields = '__all__'
+
+    def get_valor_usd(self, obj: Producto):
+        return round(obj.valor / settings.USD_TODAY, 1)
 
 
 class ProductoViewSet(ModelViewSet):
