f7ec8bbc753c49e3d4622b8566ead4072e7a54ef
danaevenegas <danaevenegasmaldonado@gmail.com>
2024-05-20 17:30:39 -0400

    timedelta access token

--- ferremax/settings.py
+++ ferremax/settings.py
@@ -167,7 +167,7 @@ WEBPAY_PLUS_CONFIG = {
 # Configuracion autenticacion
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
